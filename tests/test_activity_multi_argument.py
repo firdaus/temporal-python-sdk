546f701ce07ef0f8d68ee73b43a14f26b4af1f6e
Mohammed Firdaus <firdaus.halim@gmail.com>
2020-10-30 14:43:34 +0800

    Add basic test cases for activities.

---
+++ tests/test_activity_multi_argument.py
@@ -0,0 +1,46 @@
+import pytest
+from datetime import timedelta
+
+from temporal.workflow import workflow_method, WorkflowClient, Workflow
+from temporal.activity_method import activity_method
+
+TASK_QUEUE = "test_activity_multi_argument"
+DOMAIN = "default"
+
+
+class GreetingActivities:
+    @activity_method(task_queue=TASK_QUEUE, schedule_to_close_timeout=timedelta(seconds=1000))
+    async def compose_greeting(self, arg1, arg2) -> str:
+        raise NotImplementedError
+
+
+class GreetingActivitiesImpl:
+
+    def compose_greeting(self, arg1, arg2):
+        return arg1 + arg2
+
+
+class GreetingWorkflow:
+    @workflow_method(task_queue=TASK_QUEUE)
+    async def get_greeting(self) -> None:
+        raise NotImplementedError
+
+
+class GreetingWorkflowImpl(GreetingWorkflow):
+
+    def __init__(self):
+        self.greeting_activities: GreetingActivities = Workflow.new_activity_stub(GreetingActivities)
+
+    async def get_greeting(self):
+        return await self.greeting_activities.compose_greeting("a", "b")
+
+
+@pytest.mark.asyncio
+@pytest.mark.worker_config(DOMAIN, TASK_QUEUE, activities=[(GreetingActivitiesImpl(), "GreetingActivities")],
+                           workflows=[GreetingWorkflowImpl])
+async def test(worker):
+    client = WorkflowClient.new_client(namespace=DOMAIN)
+    greeting_workflow: GreetingWorkflow = client.new_workflow_stub(GreetingWorkflow)
+    ret_value = await greeting_workflow.get_greeting()
+
+    assert ret_value == "ab"
