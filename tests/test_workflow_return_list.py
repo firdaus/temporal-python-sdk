b4da2cdf1e1b2b7012019d76bbbd88965bcc6c48
Mohammed Firdaus <firdaus.halim@gmail.com>
2020-11-08 03:49:22 +0800

    Test cases for workflows and activities that return lists.

---
+++ tests/test_workflow_return_list.py
@@ -0,0 +1,28 @@
+import pytest
+
+from temporal.workflow import workflow_method, WorkflowClient
+
+TASK_QUEUE = "test_workflow_return_list_tq"
+NAMESPACE = "default"
+
+
+class GreetingWorkflow:
+    @workflow_method(task_queue=TASK_QUEUE)
+    async def get_greeting(self) -> None:
+        raise NotImplementedError
+
+
+class GreetingWorkflowImpl(GreetingWorkflow):
+
+    async def get_greeting(self):
+        return ["a", "b"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.worker_config(NAMESPACE, TASK_QUEUE, activities=[], workflows=[GreetingWorkflowImpl])
+async def test(worker):
+    client = WorkflowClient.new_client(namespace=NAMESPACE)
+    greeting_workflow: GreetingWorkflow = client.new_workflow_stub(GreetingWorkflow)
+    return_value = await greeting_workflow.get_greeting()
+
+    assert return_value == ["a", "b"]
