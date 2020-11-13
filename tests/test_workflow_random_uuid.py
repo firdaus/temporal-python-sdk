1661913e915d4a586ebc94a93d1be81fc191d945
Mohammed Firdaus <firdaus.halim@gmail.com>
2020-11-13 23:12:20 +0800

    Made these test cases repeatable.

--- tests/test_workflow_random_uuid.py
+++ tests/test_workflow_random_uuid.py
@@ -33,6 +33,8 @@ class GreetingWorkflowImpl(GreetingWorkflow):
 @pytest.mark.asyncio
 @pytest.mark.worker_config(NAMESPACE, TASK_QUEUE, activities=[], workflows=[GreetingWorkflowImpl])
 async def test(worker):
+    global a, b, c
+    a, b, c = [], [], []
     client = WorkflowClient.new_client(namespace=NAMESPACE)
     greeting_workflow: GreetingWorkflow = client.new_workflow_stub(GreetingWorkflow)
     await greeting_workflow.get_greeting()
