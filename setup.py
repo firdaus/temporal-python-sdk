fdf7c1e1feba211bd1fb35f4242d16d4ef33696c
Mohammed Firdaus <firdaus.halim@gmail.com>
2021-03-02 13:21:27 +0800

    Clean up dependencies and move them to setup.py.

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from pkg_resources import parse_requirements
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_reqs = parse_requirements("requirements.txt")
-
 setuptools.setup(
     name="temporal-python-sdk",
     version="1.0.0-beta1",
@@ -17,6 +15,14 @@ setuptools.setup(
     url="https://github.com/firdaus/temporal-python-sdk",
     packages=setuptools.find_packages(exclude=["cadence", "tests", "cadence.*", "tests.*"]),
     install_requires=[
+        "betterproto @ git+https://github.com/firdaus/python-betterproto@f7e9ccc6dab6cd42626ad420e3ebfe2a79b9f699",
+        "dataclasses-json==0.3.8",
+        "grpcio==1.30.0",
+        "grpclib==0.3.2",
+        "h2==3.2.0",
+        "more-itertools==7.0.0",
+        "pytz==2020.1",
+        "tblib==1.6.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
