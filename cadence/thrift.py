bbac0654a629cdf9d3c20ae082d127744bea08e2
Chad Greenburg <chad.greenburg@optimally.com>
2021-04-09 13:51:32 -0500

    Removing cadence from the repository.

--- cadence/thrift.py
+++
@@ -1,7 +0,0 @@
-import os
-
-import thriftrw
-
-this_dir = os.path.dirname(__file__)
-cadence_thrift_file = os.path.join(this_dir, "thrift/cadence.thrift")
-cadence_thrift = thriftrw.load(cadence_thrift_file)
